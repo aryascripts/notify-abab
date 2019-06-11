7468a910a339fcc261b2043c69f1ff394b2a4fb2
Aman Bhimani <amanb014@gmail.com>
2019-06-11 19:30:54 -0400

    Base App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,8 @@ import React from "react";
 import styled from "@emotion/styled";
 import "./app.css";
 
-import Greeting from "./components/Greeting";
-import Dependencies from "./components/Dependencies";
+import Header from "./components/common/Header";
 
 export default function App() {
-  return (
-    <Centered>
-      <Split>
-        <Panel className="u-flexV u-centerBoth">
-          <Greeting />
-        </Panel>
-        <Panel>
-          <Dependencies />
-        </Panel>
-      </Split>
-    </Centered>
-  );
+  return <Header />;
 }
-
-const Centered = styled.div`
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  height: 100%;
-`;
-
-const Split = styled.div`
-  display: grid;
-  grid-template-columns: 40% auto;
-  width: 80%;
-  grid-gap: 30px;
-`;
-
-const Panel = styled.div`
-  background: #fff;
-  padding: 30px;
-  border-radius: 10px;
-  box-shadow: 0 8px 19px hsla(0, 0%, 0%, 0.06);
-  height: 500px;
-  overflow: auto;
-`;
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+export default function Header() {
+  return <TopPanel>Notify</TopPanel>;
+}
+
+const TopPanel = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  font-family: monospace;
+  text-transform: uppercase;
+
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  background-color: #f0f4f8;
+  width: 100%;
+  height: 35px;
+`;
