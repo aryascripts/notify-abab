98fd9fa76d4a98990e2095544f651d6da575bfb1
Aman Bhimani <amanb014@gmail.com>
2019-06-14 18:35:00 -0400

    moved to parent dir

--- create-react-app/notefy-abab/src/firebase.config.ts
+++
@@ -1,7 +0,0 @@
-export const config = {
-  authDomain: "notify-abab.firebaseapp.com",
-  databaseURL: "https://notify-abab.firebaseio.com",
-  projectId: "notify-abab",
-  storageBucket: "notify-abab.appspot.com",
-  messagingSenderId: "1060683541608",
-};
--- rtl.setup.js
+++
@@ -1,4 +0,0 @@
-// common imports for react-testing-library tests
-import "react-testing-library/cleanup-after-each";
-import "react-hooks-testing-library/cleanup-after-each";
-import "@babel/polyfill";
