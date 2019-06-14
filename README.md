c9c3c7c8e05ce2016964142df429801199b88b61
Aman Bhimani <amanb014@gmail.com>
2019-06-14 18:30:23 -0400

    create-react-app

--- README.md
+++
@@ -1,7 +0,0 @@
-# notify-abab
-
-Notes application for Aman.
-
-# References
-
-[React TS Starter](https://github.com/GeordieP/react-ts-starter) - GeordieP
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import "normalize.css";
-import "./util.css";
+
 
 import App from "./App";
 render(<App />, document.getElementById("root"));
